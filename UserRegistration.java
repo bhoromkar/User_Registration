073efb08cd354d39c522cf7020cb9431596261d9
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-05-01 19:31:19 +0530

    User_Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,10 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    static Pattern pattern = Pattern.compile("^(?=.*[A-Z])[a-zA-Z0-9.,@#$%^&*><|_%+-]{8,}+$");
+    static Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])[a-zA-Z0-9.,@#$%^&*><|_%+-]{8,}+$");
 
     public static void main(String[] args) {
-        String password= "dlnaaaaA568mdvlm";
+        String password= "GHJKUYGIaHA568";
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
             System.out.println("Valid");
