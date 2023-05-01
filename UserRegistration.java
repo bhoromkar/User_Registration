ccc2e00b4c1e0815aaabbfbe8137e9610ef4c72e
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-05-01 19:07:23 +0530

    User_Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,11 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    static Pattern pattern = Pattern.compile("^[A-Z]+[a-z]{2,}+$");
+    static Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+\\.+[A-Za-z0-9+_.-]+@(?:[a-z])+(?:\\.[a-zA-Z]{2})+(?:\\.[a-zA-Z]{2})?$");
 
     public static void main(String[] args) {
-        String lastName= "Abhc";
-        Matcher matcher = pattern.matcher(lastName);
+        String Email= "abc.xyz@bl.co.in";
+        Matcher matcher = pattern.matcher(Email);
         if (matcher.matches()) {
             System.out.println("Valid");
         }
