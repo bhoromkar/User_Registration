b401ba681a4e659f0f2d523e9bd72ae7d0c8f4d9
bhoromkar <118914116+bhoromkar@users.noreply.github.com>
2023-05-02 19:25:00 +0530

    User_ValidationByLamda

--- MailValidation.java
+++ MailValidation.java
@@ -1,2 +1,20 @@
-package com.bridgelabz.userresgistration;public class MailValidation {
+package com.bridgelabz.userresgistration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class MailValidation {
+    //static Pattern pattern = Pattern.compile("^[A-Za-z0-9+_.-]+\\.+[A-Za-z0-9+_.-]+@(?:[a-z])+[(\\.[a-zA-Z]{2})+(\\.[a-zA-Z]{2})?]$");
+    static Pattern pattern = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+
+    public static void main(String[] args) {
+        String Email= "abc.100@abc.com.au";
+        Matcher matcher = pattern.matcher(Email);
+        if (matcher.matches()) {
+            System.out.println("Valid");
+        }
+        else {
+            System.out.println("Invalid");
+        }
+    }
 }
+
+
--- UserRegistration.java
+++ UserRegistration.java
@@ -7,7 +7,7 @@ public class UserRegistration {
     static Pattern pattern = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[!@#$%^&*><])[a-zA-Z0-9.,@#$%^&*><|_%+-]{8,}+$");
 
     public static void main(String[] args) {
-        String password= "GHJKUYGIaHA65@2@";
+        String password= "GHJKUYGIaHA65";
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
             System.out.println("Valid");
